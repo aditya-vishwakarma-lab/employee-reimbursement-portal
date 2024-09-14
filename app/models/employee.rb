77c89f908fc192241619c2b1f3c37a36ddb31e19
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2024-09-14 14:28:01 +0530

    Add CRUD for employees
    Now user can CRUD employees

---
+++ app/models/employee.rb
@@ -0,0 +1,4 @@
+class Employee < ApplicationRecord
+  validates :name, presence: true
+  belongs_to :company
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_14_073930) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_14_082327) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.integer "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_073930) do
   end
 
   add_foreign_key "companies", "users"
+  add_foreign_key "employees", "companies"
 end
