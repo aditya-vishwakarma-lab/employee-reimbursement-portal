9e24a989a86eb259ef025762b734116ed18a85ec
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2024-09-14 20:34:44 +0530

    Change db from SQLlite to pgSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_09_14_112356) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,7 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_112356) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_companies_on_user_id"
@@ -49,14 +52,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_112356) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
-    t.integer "company_id", null: false
+    t.bigint "company_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
   create_table "reimbursement_claims", force: :cascade do |t|
-    t.integer "employee_id", null: false
+    t.bigint "employee_id", null: false
     t.text "purpose"
     t.decimal "amount"
     t.date "date_of_expenditure"
