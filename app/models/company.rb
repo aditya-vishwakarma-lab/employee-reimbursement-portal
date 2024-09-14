6cf8ae45717836bd564702ce2233f499733d9b85
Aditya Kumar Vishwakarma <vishwakarmaaditya03@gmail.com>
2024-09-14 16:14:41 +0530

    Associate reimbursement claim with companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
   has_many :employees
+  has_many :reimbursement_claims, through: :employees
 end
--- app/models/reimbursement_claim.rb
+++ app/models/reimbursement_claim.rb
@@ -1,4 +1,5 @@
 class ReimbursementClaim < ApplicationRecord
   validates :amount, numericality: { greater_than_or_equal_to: 0, message: "must be a positive number" }
   belongs_to :employee
+  has_one :company, through: :employee
 end
